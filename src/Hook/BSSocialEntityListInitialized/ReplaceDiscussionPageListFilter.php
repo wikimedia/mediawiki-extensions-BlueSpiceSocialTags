d8bfe927c699fa559439803ca4c02ff81e956ea4
pwirth <wirth@hallowelt.biz>
2018-09-04 15:46:29 +0200

    Add hookhandler to replace current filters for discussion pagees

---
+++ src/Hook/BSSocialEntityListInitialized/ReplaceDiscussionPageListFilter.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace BlueSpice\Social\Tags\Hook\BSSocialEntityListInitialized;
+
+use BlueSpice\Social\Hook\BSSocialEntityListInitialized;
+use BlueSpice\Data\FieldType;
+use BlueSpice\Data\Filter\Numeric;
+use BlueSpice\Data\Filter\ListValue;
+use BlueSpice\Social\Topics\Entity\Topic;
+use BlueSpice\Social\Renderer\EntityList;
+use BlueSpice\Social\Topics\EntityListContext\DiscussionPage;
+
+class ReplaceDiscussionPageListFilter extends BSSocialEntityListInitialized {
+	protected function skipProcessing() {
+		if( !$this->entityList->getContext() instanceof DiscussionPage ) {
+			return true;
+		}
+		return false;
+	}
+
+	protected function doProcess() {
+		$this->args[EntityList::PARAM_FILTER] = array_filter(
+			$this->args[EntityList::PARAM_FILTER],
+			function( $e ) {
+				return $e->{Numeric::KEY_PROPERTY} !== Topic::ATTR_DISCUSSION_TITLE_ID;
+			}
+		);
+		$this->args[EntityList::PARAM_FILTER][] = (object)[
+			ListValue::KEY_PROPERTY => 'tags',
+			ListValue::KEY_VALUE => ['Hauptseite', 'Talk:Hauptseite'],
+			ListValue::KEY_COMPARISON => ListValue::COMPARISON_CONTAINS,
+			ListValue::KEY_TYPE => FieldType::LISTVALUE
+		];
+		foreach( $this->args[EntityList::PARAM_LOCKED_FILTER_NAMES] as &$name ) {
+			if( $name !== Topic::ATTR_DISCUSSION_TITLE_ID ) {
+				continue;
+			}
+			unset( $name );
+			return;
+		}
+		$this->args[EntityList::PARAM_LOCKED_FILTER_NAMES][] = 'tags';
+	}
+
+}

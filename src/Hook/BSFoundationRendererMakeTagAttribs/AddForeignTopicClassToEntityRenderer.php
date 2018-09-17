6accf490338e3c70b79fcf9fd3e73e8b50e70947
pwirth <wirth@hallowelt.biz>
2018-09-17 13:54:47 +0200

    Add class and styles for foreign duscussion topics ERM:10596

---
+++ src/Hook/BSFoundationRendererMakeTagAttribs/AddForeignTopicClassToEntityRenderer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BlueSpice\Social\Tags\Hook\BSFoundationRendererMakeTagAttribs;
+
+use BlueSpice\Hook\BSFoundationRendererMakeTagAttribs;
+use BlueSpice\Social\Renderer\Entity;
+use BlueSpice\Social\Topics\EntityListContext\DiscussionPage;
+use BlueSpice\Social\Topics\EntityListContext\AfterContent;
+
+class AddForeignTopicClassToEntityRenderer extends BSFoundationRendererMakeTagAttribs {
+
+	protected function skipProcessing() {
+		if( !$this->renderer instanceof Entity ) {
+			return true;
+		}
+		if( $this->renderer->getEntity()->getType() !== 'topic' ) {
+			return true;
+		}
+		if( !$this->renderer->getContext() instanceof DiscussionPage
+			/*&& !$this->renderer->getContext() instanceof AfterContent*/ ) {
+			//currently foreign topics will not be displayed in after article
+			//content
+			return true;
+		}
+
+		$title = $this->renderer->getEntity()->getRelatedTitle()->getTalkPage();
+		$ctxTitle = $this->renderer->getContext()->getTitle()->getTalkPage();
+
+		if( !$title || !$ctxTitle ) {
+			return true;
+		}
+		if( $title->equals( $ctxTitle ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	protected function doProcess() {
+		if( empty( $this->attribs['class'] ) ) {
+			$this->attribs['class'] = '';
+		}
+		$this->attribs['class'] .= ' foreign';
+	}
+
+}

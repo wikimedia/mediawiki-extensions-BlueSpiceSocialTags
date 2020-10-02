d174c57b32ac84df6b302a9fa103e366a84c5c70
pwirth <wirth@hallowelt.biz>
2020-10-02 09:53:06 +0200

    Fix tags button and add icon
    ERM:20227
    [3.3]
    
    Change-Id: Ie2895126ce57d6f205861f3be9c98c1db253b253

--- resources/EntityActionMenu/Tags.js
+++ resources/EntityActionMenu/Tags.js
@@ -13,11 +13,11 @@ bs.social.EntityActionMenuTags.Tags = function ( entityActionMenu, data ) {
 	var me = this;
 	me.data = data || {};
 	me.entityActionMenu = entityActionMenu;
-	var countTags = me.entityActionMenu.entity.data.values.tags.length;
+	var countTags = me.entityActionMenu.entity.data.get( 'tags', [] ).length;
 
 	me.$element = null;
-	me.$element = $( '<li><a class="dropdown-item bs-social-entityaftercontent-tags">'
-		+ mw.message( 'bs-socialtags-tagstext', countTags ).text()
+	me.$element = $( '<li><a class="dropdown-item bs-social-entity-action-tags">'
+		+ '<span>' + mw.message( 'bs-socialtags-tagstext', countTags ).text() + '</span>'
 		+ '</a></li>'
 	);
 
@@ -28,78 +28,77 @@ bs.social.EntityActionMenuTags.Tags = function ( entityActionMenu, data ) {
 OO.initClass( bs.social.EntityActionMenuTags.Tags );
 OO.mixinClass( bs.social.EntityActionMenuTags.Tags, OO.EventEmitter );
 
-bs.social.EntityActionMenuTags.Tags.prototype.click = function (e) {
+bs.social.EntityActionMenuTags.Tags.prototype.click = function ( e ) {
+	e.preventDefault();
 	var me = this;
-	var menuEntity = me.entityActionMenu.entity;
-
-	if( !menuEntity.getConfig().isTagable ) {
+	var entity = me.entityActionMenu.entity;
+	if( !entity.getConfig().IsTagable ) {
 		return;
 	}
-	if( menuEntity.hasParent() ) {
+	if( entity.hasParent() ) {
 		return;
 	}
-
-	if( !menuEntity.$tagEditor ) {
-		menuEntity.$tagEditor = $(
+	if( !entity.$tagEditor ) {
+		entity.$tagEditor = $(
 			'<div class="bs-social-entity-aftercontent-tageditor"></div>'
 		).hide();
-		menuEntity.$tagEditor.insertAfter(
-			menuEntity.getEl().find('.bs-social-entity-aftercontent').first()
+		entity.$tagEditor.insertAfter(
+			entity.getEl().find('.bs-social-entity-aftercontent').first()
 		);
-		menuEntity.$tagSelection = $(
+		entity.$tagSelection = $(
 			'<div><select style="width:100%"></select></div>'
 		);
-		menuEntity.tagSubmitButton = new OO.ui.ButtonInputWidget( {
+		entity.tagSubmitButton = new OO.ui.ButtonInputWidget( {
 			label: mw.message( 'bs-social-editor-ok' ).plain(),
 			flags: ['progressive', 'primary'],
 			align: 'right'
 		});
-		menuEntity.tagCancelButton = new OO.ui.ButtonInputWidget( {
+		entity.tagCancelButton = new OO.ui.ButtonInputWidget( {
 			label: mw.message( 'bs-social-editor-cancel' ).plain(),
 			align: 'right'
 		});
-		menuEntity.tagSubmitButton.on( 'click', function() {
-			var val = menuEntity.$tagSelection.find( 'select' ).select2( "val" );
+		entity.tagSubmitButton.on( 'click', function() {
+			var val = entity.$tagSelection.find( 'select' ).select2( "val" );
 			if( !val ) {
 				val = [];
 			}
-			menuEntity.showLoadMask();
+			entity.showLoadMask();
 			bs.api.tasks.execSilent(
 				'socialtags',
 				'editTags',
-				{ id: menuEntity.id,
-					type: menuEntity.type,
+				{ id: entity.id,
+					type: entity.type,
 					tags: val
 				}
 			).done( function( response ) {
-				menuEntity.replaceEL( response.payload.view );
+				entity.replaceEL( response.payload.view );
 			})
 			.then(function(){
-				menuEntity.hideLoadMask();
+				entity.hideLoadMask();
 			});
 		});
-		menuEntity.tagCancelButton.on( 'click', function() {
-			menuEntity.$tagEditor.toggle();
-			menuEntity.$tagEditor.remove();
-			menuEntity.$tagEditor = null;
+		entity.tagCancelButton.on( 'click', function() {
+			entity.$tagEditor.toggle();
+			entity.$tagEditor.remove();
+			entity.$tagEditor = null;
 		});
 
-		var items = [], tags = menuEntity.data.get( 'tags' , []);
+		var items = [], tags = entity.data.get( 'tags' , []);
 		for( var i = 0; i < tags.length; i++ ) {
 			items.push( { id: tags[i], text: tags[i] });
 		}
 
-		menuEntity.$tagEditor.append (
-			menuEntity.$tagSelection
+		entity.$tagEditor.append (
+			entity.$tagSelection
 		);
-		menuEntity.$tagEditor.append(
-			menuEntity.tagCancelButton.$element
+		entity.$tagEditor.append(
+			entity.tagCancelButton.$element
 		);
-		menuEntity.$tagEditor.append(
-			menuEntity.tagSubmitButton.$element
+		entity.$tagEditor.append(
+			entity.tagSubmitButton.$element
 		);
 
-		var $select2 = menuEntity.$tagSelection.find( 'select' ).select2({
+		var $select2 = entity.$tagSelection.find( 'select' ).select2({
 			multiple: true,
 			allowClear: true,
 			ajax: {
@@ -138,7 +137,6 @@ bs.social.EntityActionMenuTags.Tags.prototype.click = function (e) {
 		$select2.trigger( 'change' );
 	}
 
-	menuEntity.$tagEditor.toggle();
-	e.preventDefault();
+	entity.$tagEditor.toggle();
 	return false;
 };
